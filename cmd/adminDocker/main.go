c177e8d2b9f755247684c5831e2c750e794d4f18
laura.kalmanova <bubamarley7@gmail.com>
2025-02-19 15:47:32 +0100

    Add

--- cmd/adminDocker/main.go
+++ cmd/adminDocker/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"adminDocker/app/routes/dockers"
 	"adminDocker/app/server"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
 	"os"
 
 	"github.com/rs/zerolog/log"
+
+	controllers "adminDocker/app/controllers/common"
+	routes "adminDocker/app/routes/common"
 )
 
 func main() {
@@ -16,4 +23,48 @@ func main() {
 
 	srv := server.GetServer()
 	srv.ListenAndServe()
+}
+
+func newAdminDockerServer() error {
+
+	// loading .env files in dev mode
+	if os.Getenv("MODE") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			return err
+		}
+	}
+
+	srv := &server.AdminDocker{}
+
+	srv.ParseParameters()
+
+	// log format definition
+	switch srv.LogFormat {
+	case "HUMAN":
+		log.Logger = log.Logger.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	case "JSON":
+		// Already default
+	default:
+		log.Logger = log.Logger.Output(zerolog.ConsoleWriter{Out: os.Stderr, NoColor: true})
+	}
+
+	// setup router
+	srv.Router = setupRouter()
+
+	err := dockers.SetupRouter(srv.Router, &log.Logger)
+	if err != nil {
+		return err
+	}
+	server.SetServer(srv)
+
+	return nil
+}
+
+func setupRouter() *gin.Engine {
+	router := routes.SetupRouter()
+	router.GET("/ping", controllers.Ping)
+	router.GET("/version", controllers.Version)
+
+	return router
 }
